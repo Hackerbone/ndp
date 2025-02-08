3b78f8906386249f3f390b6ea21371e5859ec8e6
hackerbone <sitaraman.raghav.india@gmail.com>
2025-02-08 11:07:57 +0530

    Lab 6 and 5

--- lab2/tcp_client.c
+++ lab2/tcp_client.c
@@ -5,17 +5,19 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define PORT 8081
 #define MAX_BUFFER 1024
 
-int main() {
+int main()
+{
     int sockfd;
     struct sockaddr_in server_addr;
     char buffer[MAX_BUFFER];
     char filename[MAX_BUFFER];
 
     // Create socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -25,28 +27,31 @@ int main() {
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     // Connect to server
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
     // Get filename from user
     printf("Enter the filename: ");
-    fgets(filename, sizeof(filename), stdin);
+    scanf("%s", filename);
     filename[strcspn(filename, "\n")] = 0; // Remove newline character
-
-    send(sockfd, filename, strlen(filename), 0);
+    printf("Sending filename: %s\n", filename);
+    send(sockfd, filename, strlen(filename) + 1, 0); // Send null-terminated string
 
     // Receive response from server
     recv(sockfd, buffer, sizeof(buffer), 0);
     printf("%s\n", buffer);
 
-    if (strncmp(buffer, "File not present", 17) == 0) {
+    if (strncmp(buffer, "File not present", 17) == 0)
+    {
         close(sockfd);
         return 0;
     }
 
-    while (1) {
+    while (1)
+    {
         printf("Enter your choice (1- Search, 2- Replace, 3- Reorder, 4- Exit): ");
         int choice;
         scanf("%d", &choice);
@@ -54,7 +59,8 @@ int main() {
 
         send(sockfd, &choice, sizeof(choice), 0);
 
-        if (choice == 1) { // Search
+        if (choice == 1)
+        { // Search
             char search_str[MAX_BUFFER];
             printf("Enter string to search: ");
             fgets(search_str, sizeof(search_str), stdin);
@@ -62,7 +68,9 @@ int main() {
             send(sockfd, search_str, strlen(search_str), 0);
             recv(sockfd, buffer, sizeof(buffer), 0);
             printf("%s\n", buffer);
-        } else if (choice == 2) { // Replace
+        }
+        else if (choice == 2)
+        { // Replace
             char old_str[MAX_BUFFER], new_str[MAX_BUFFER];
             printf("Enter string to replace: ");
             fgets(old_str, sizeof(old_str), stdin);
@@ -74,10 +82,14 @@ int main() {
             send(sockfd, new_str, strlen(new_str), 0);
             recv(sockfd, buffer, sizeof(buffer), 0);
             printf("%s\n", buffer);
-        } else if (choice == 3) { // Reorder
+        }
+        else if (choice == 3)
+        { // Reorder
             recv(sockfd, buffer, sizeof(buffer), 0);
             printf("%s\n", buffer);
-        } else if (choice == 4) { // Exit
+        }
+        else if (choice == 4)
+        { // Exit
             break;
         }
     }
--- lab2/tcp_server.c
+++ lab2/tcp_server.c
@@ -6,20 +6,24 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#define PORT 8080
+#define PORT 8081
 #define MAX_BUFFER 1024
 
 // Function to search a string in the file
-int search_string_in_file(char *filename, char *search_str) {
+int search_string_in_file(char *filename, char *search_str)
+{
     FILE *file = fopen(filename, "r");
-    if (!file) {
+    if (!file)
+    {
         return -1; // File not found
     }
 
     char line[MAX_BUFFER];
     int count = 0;
-    while (fgets(line, sizeof(line), file)) {
-        if (strstr(line, search_str)) {
+    while (fgets(line, sizeof(line), file))
+    {
+        if (strstr(line, search_str))
+        {
             count++;
         }
     }
@@ -29,18 +33,22 @@ int search_string_in_file(char *filename, char *search_str) {
 }
 
 // Function to replace string in the file
-int replace_string_in_file(char *filename, char *old_str, char *new_str) {
+int replace_string_in_file(char *filename, char *old_str, char *new_str)
+{
     FILE *file = fopen(filename, "r+");
-    if (!file) {
+    if (!file)
+    {
         return -1; // File not found
     }
 
     char temp[MAX_BUFFER];
     long pos;
     int found = 0;
-    while (fgets(temp, sizeof(temp), file)) {
+    while (fgets(temp, sizeof(temp), file))
+    {
         pos = ftell(file);
-        if (strstr(temp, old_str)) {
+        if (strstr(temp, old_str))
+        {
             found = 1;
             // Replace occurrence
             fseek(file, pos - strlen(temp), SEEK_SET);
@@ -53,9 +61,11 @@ int replace_string_in_file(char *filename, char *old_str, char *new_str) {
 }
 
 // Function to reorder lines in file based on ASCII value
-int reorder_file_content(char *filename) {
+int reorder_file_content(char *filename)
+{
     FILE *file = fopen(filename, "r+");
-    if (!file) {
+    if (!file)
+    {
         return -1; // File not found
     }
 
@@ -64,15 +74,19 @@ int reorder_file_content(char *filename) {
     char *lines[MAX_BUFFER];
     int line_count = 0;
 
-    while (fgets(temp, sizeof(temp), file)) {
+    while (fgets(temp, sizeof(temp), file))
+    {
         lines[line_count] = strdup(temp);
         line_count++;
     }
 
     // Sort lines based on ASCII value
-    for (int i = 0; i < line_count - 1; i++) {
-        for (int j = i + 1; j < line_count; j++) {
-            if (strcmp(lines[i], lines[j]) > 0) {
+    for (int i = 0; i < line_count - 1; i++)
+    {
+        for (int j = i + 1; j < line_count; j++)
+        {
+            if (strcmp(lines[i], lines[j]) > 0)
+            {
                 char *temp_line = lines[i];
                 lines[i] = lines[j];
                 lines[j] = temp_line;
@@ -81,7 +95,8 @@ int reorder_file_content(char *filename) {
     }
 
     fseek(file, 0, SEEK_SET);
-    for (int i = 0; i < line_count; i++) {
+    for (int i = 0; i < line_count; i++)
+    {
         fputs(lines[i], file);
         free(lines[i]);
     }
@@ -90,7 +105,8 @@ int reorder_file_content(char *filename) {
     return 0;
 }
 
-int main() {
+int main()
+{
     int server_fd, new_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
@@ -98,7 +114,8 @@ int main() {
     char filename[MAX_BUFFER];
 
     // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
@@ -107,70 +124,106 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", PORT);
 
-    while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0)
+        {
             perror("Accept failed");
             exit(EXIT_FAILURE);
         }
 
-        read(new_socket, buffer, sizeof(buffer));
+        int bytes_read = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_read < 0)
+        {
+            perror("Read failed");
+            close(new_socket);
+            continue;
+        }
+        buffer[bytes_read] = '\0'; // Ensure null-termination
+
+        // Trim any trailing newline or carriage return characters
+        buffer[strcspn(buffer, "\r\n")] = '\0';
+
         strncpy(filename, buffer, sizeof(filename));
+        filename[sizeof(filename) - 1] = '\0'; // Ensure null-termination
+
+        printf("Received filename: %s\n", filename); // Debugging statement
 
         FILE *file = fopen(filename, "r");
-        if (!file) {
+        if (!file)
+        {
+            perror("File open failed"); // Debugging statement
             send(new_socket, "File not present", 17, 0);
             close(new_socket);
             continue;
         }
 
         send(new_socket, "File found. Select an option:\n1. Search\n2. Replace\n3. Reorder\n4. Exit", 75, 0);
-        
+
         int choice;
-        while (1) {
+        while (1)
+        {
             recv(new_socket, &choice, sizeof(choice), 0);
-            if (choice == 4) {
+            if (choice == 4)
+            {
                 close(new_socket);
                 break;
             }
 
-            if (choice == 1) { // Search
+            if (choice == 1)
+            { // Search
                 char search_str[MAX_BUFFER];
                 recv(new_socket, search_str, sizeof(search_str), 0);
                 int count = search_string_in_file(filename, search_str);
-                if (count >= 0) {
+                if (count >= 0)
+                {
                     char result[MAX_BUFFER];
                     snprintf(result, sizeof(result), "Found %d occurrences", count);
                     send(new_socket, result, strlen(result), 0);
-                } else {
+                }
+                else
+                {
                     send(new_socket, "String not found", 17, 0);
                 }
-            } else if (choice == 2) { // Replace
+            }
+            else if (choice == 2)
+            { // Replace
                 char old_str[MAX_BUFFER], new_str[MAX_BUFFER];
                 recv(new_socket, old_str, sizeof(old_str), 0);
                 recv(new_socket, new_str, sizeof(new_str), 0);
                 int result = replace_string_in_file(filename, old_str, new_str);
-                if (result == 1) {
+                if (result == 1)
+                {
                     send(new_socket, "String replaced", 15, 0);
-                } else {
+                }
+                else
+                {
                     send(new_socket, "String not found", 17, 0);
                 }
-            } else if (choice == 3) { // Reorder
+            }
+            else if (choice == 3)
+            { // Reorder
                 int result = reorder_file_content(filename);
-                if (result == 0) {
+                if (result == 0)
+                {
                     send(new_socket, "File reordered", 14, 0);
-                } else {
+                }
+                else
+                {
                     send(new_socket, "Error reordering file", 21, 0);
                 }
             }
